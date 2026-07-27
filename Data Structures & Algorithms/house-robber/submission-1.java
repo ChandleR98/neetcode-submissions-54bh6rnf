272acdeccdcbcad0d7832a989060f0c95010f465
ChandleR98 <68632845+ChandleR98@users.noreply.github.com>
2026-07-27 11:55:16 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if (n == 1) return nums[0];
+        int[] dp=new int[n];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            int nontake=dp[i-1];
+            
+            int take=nums[i]+dp[i-2];
+            dp[i]=Math.max(take,nontake);
+        }
+        return dp[n-1];
+    }
+}
